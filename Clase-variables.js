147a32914f4388669706e44636f8dcc85b70f1ae
Byron Fernando Cardona Sánchez <68670367+byron23-creator@users.noreply.github.com>
2024-05-20 21:15:16 -0600

    Add files via upload

---
+++ Clase-variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo no permitido
+let c = 'Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+ //let c, cda
+    pcAndy 
+
+// Lo permitido
+    let primerTrasteoDeAndy = 'woody'
+    let urlDelUsuario= 'https://www.google.com'
+    let idDleUsuario = '12344';
